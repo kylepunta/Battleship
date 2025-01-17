66717ae66c02ce277a59bb864e43cc1200006da4
kylepunta <kylepurcell1998@gmail.com>
2025-01-17 20:07:09 +0000

    Modified addShipEventListeners and addFleetSquaresEventListeners functions

--- eventListeners.js
+++ eventListeners.js
@@ -1,53 +1,167 @@
-import { createDOMQueries, DOMState } from "./DOM.js";
+import { DOMQueries, DOMState, renderDOM } from "./DOM.js";
+import { gameState } from "./main.js";
+import { Ship } from "./Ship.js";
 
 const eventHandler = function () {
-  const DOMQueries = createDOMQueries();
   const addShipEventListeners = function () {
-    DOMQueries.carrier.addEventListener("click", () => {
-      console.log("clicked!");
-      DOMState.setActiveShip("carrier");
+    DOMQueries.carriers.forEach((carrier, index) => {
+      carrier.addEventListener("click", () => {
+        DOMQueries.shipContainers.forEach((shipContainer) => {
+          shipContainer.classList.remove("disabled");
+        });
+        if (index === 0) {
+          if (DOMState.getPlayerOneActiveShip() !== null) {
+            renderDOM.clearHighlightedShips(DOMState.getPlayerOneActiveShip());
+          }
+          DOMState.setPlayerOneActiveShip({ type: "carrier", length: 5 });
+          DOMQueries.shipContainers[0].classList.add("disabled");
+        } else {
+          if (DOMState.getPlayerTwoActiveShip() !== null) {
+            renderDOM.clearHighlightedShips(DOMState.getPlayerTwoActiveShip());
+          }
+          DOMState.setPlayerTwoActiveShip({ type: "carrier", length: 5 });
+          DOMQueries.shipContainers[5].classList.add("disabled");
+        }
+      });
     });
-    DOMQueries.battleship.addEventListener("click", () => {
-      console.log("clicked!");
-      DOMState.setActiveShip("battleship");
+    DOMQueries.battleships.forEach((battleship, index) => {
+      battleship.addEventListener("click", () => {
+        DOMQueries.shipContainers.forEach((shipContainer) => {
+          shipContainer.classList.remove("disabled");
+        });
+        if (index === 0) {
+          if (DOMState.getPlayerOneActiveShip() !== null) {
+            renderDOM.clearHighlightedShips(DOMState.getPlayerOneActiveShip());
+          }
+          DOMState.setPlayerOneActiveShip({ type: "battleship", length: 4 });
+          DOMQueries.shipContainers[1].classList.add("disabled");
+        } else {
+          if (DOMState.getPlayerTwoActiveShip() !== null) {
+            renderDOM.clearHighlightedShips(DOMState.getPlayerTwoActiveShip());
+          }
+          DOMState.setPlayerTwoActiveShip({ type: "battleship", length: 4 });
+          DOMQueries.shipContainers[6].classList.add("disabled");
+        }
+      });
     });
-    DOMQueries.cruiser.addEventListener("click", () => {
-      console.log("clicked!");
-      DOMState.setActiveShip("cruiser");
+    DOMQueries.cruisers.forEach((cruiser, index) => {
+      cruiser.addEventListener("click", () => {
+        DOMQueries.shipContainers.forEach((shipContainer) => {
+          shipContainer.classList.remove("disabled");
+        });
+        if (index === 0) {
+          if (DOMState.getPlayerOneActiveShip() !== null) {
+            renderDOM.clearHighlightedShips(DOMState.getPlayerOneActiveShip());
+          }
+          DOMState.setPlayerOneActiveShip({ type: "cruiser", length: 3 });
+          DOMQueries.shipContainers[2].classList.add("disabled");
+        } else {
+          if (DOMState.getPlayerTwoActiveShip() !== null) {
+            renderDOM.clearHighlightedShips(DOMState.getPlayerTwoActiveShip());
+          }
+          DOMState.setPlayerTwoActiveShip({ type: "cruiser", length: 3 });
+          DOMQueries.shipContainers[7].classList.add("disabled");
+        }
+      });
     });
-    DOMQueries.submarine.addEventListener("click", () => {
-      console.log("clicked!");
-      DOMState.setActiveShip("submarine");
+    DOMQueries.submarines.forEach((submarine, index) => {
+      submarine.addEventListener("click", () => {
+        DOMQueries.shipContainers.forEach((shipContainer) => {
+          shipContainer.classList.remove("disabled");
+        });
+        if (index === 0) {
+          if (DOMState.getPlayerOneActiveShip() !== null) {
+            renderDOM.clearHighlightedShips(DOMState.getPlayerOneActiveShip());
+          }
+          DOMState.setPlayerOneActiveShip({ type: "submarine", length: 3 });
+          DOMQueries.shipContainers[3].classList.add("disabled");
+        } else {
+          if (DOMState.getPlayerTwoActiveShip() !== null) {
+            renderDOM.clearHighlightedShips(DOMState.getPlayerTwoActiveShip());
+          }
+          DOMState.setPlayerTwoActiveShip({ type: "submarine", length: 3 });
+          DOMQueries.shipContainers[8].classList.add("disabled");
+        }
+      });
     });
-    DOMQueries.destroyer.addEventListener("click", () => {
-      console.log("clicked!");
-      DOMState.setActiveShip("destroyer");
-    });
-  };
-  const addFleetBoardListeners = function () {
-    DOMQueries.squares.forEach((square) => {
-      square.addEventListener("click", () => {
-        switch (DOMState.getActiveShip()) {
-          case "carrier":
-            console.log("carrier");
-            break;
-          case "battleship":
-            console.log("battleship");
-            break;
-          case "cruiser":
-            console.log("cruiser");
-            break;
-          case "submarine":
-            console.log("submarine");
-            break;
-          case "destroyer":
-            console.log("destroyer");
-            break;
+    DOMQueries.destroyers.forEach((destroyer, index) => {
+      destroyer.addEventListener("click", () => {
+        DOMQueries.shipContainers.forEach((shipContainer) => {
+          shipContainer.classList.remove("disabled");
+        });
+        if (index === 0) {
+          if (DOMState.getPlayerOneActiveShip() !== null) {
+            renderDOM.clearHighlightedShips(DOMState.getPlayerOneActiveShip());
+          }
+          DOMState.setPlayerOneActiveShip({ type: "destroyer", length: 2 });
+          DOMQueries.shipContainers[4].classList.add("disabled");
+        } else {
+          if (DOMState.getPlayerTwoActiveShip() !== null) {
+            renderDOM.clearHighlightedShips(DOMState.getPlayerTwoActiveShip());
+          }
+          DOMState.setPlayerTwoActiveShip({ type: "destroyer", length: 2 });
+          DOMQueries.shipContainers[9].classList.add("disabled");
         }
       });
     });
   };
-  return { addShipEventListeners, addFleetBoardListeners };
+  const addFleetSquaresEventListeners = function () {
+    const playerOneGrid = Array.from({ length: 10 }, () =>
+      Array(10).fill(null)
+    );
+    const playerTwoGrid = Array.from({ length: 10 }, () =>
+      Array(10).fill(null)
+    );
+    DOMQueries.playerOneFleetSquares.forEach((square) => {
+      const row = parseInt(square.dataset.row, 10);
+      const col = parseInt(square.dataset.col, 10);
+      playerOneGrid[row][col] = square;
+    });
+    DOMQueries.playerTwoFleetSquares.forEach((square) => {
+      const row = parseInt(square.dataset.row, 10);
+      const col = parseInt(square.dataset.col, 10);
+      playerTwoGrid[row][col] = square;
+    });
+    DOMQueries.playerOneFleetSquares.forEach((square) => {
+      square.addEventListener("mouseover", () => {
+        const activeShip = DOMState.getPlayerOneActiveShip();
+        console.log("Player One Active Ship", activeShip);
+        const row = parseInt(square.dataset.row, 10);
+        const col = parseInt(square.dataset.col, 10);
+        renderDOM.highlightShipPlacement(activeShip, row, col, playerOneGrid);
+      });
+    });
+    DOMQueries.playerTwoFleetSquares.forEach((square) => {
+      square.addEventListener("mouseover", () => {
+        const activeShip = DOMState.getPlayerTwoActiveShip();
+        console.log("Player Two Active Ship", activeShip);
+        const row = parseInt(square.dataset.row, 10);
+        const col = parseInt(square.dataset.col, 10);
+        renderDOM.highlightShipPlacement(activeShip, row, col, playerTwoGrid);
+      });
+    });
+  };
+  const addRotateBtnsEventListeners = function () {
+    DOMQueries.rotateButtons[0].addEventListener("click", () => {
+      const switchDirection =
+        DOMState.getPlayerOneShipPlacementDirection() === "vertical"
+          ? "horizontal"
+          : "vertical";
+      DOMState.setPlayerOneShipPlacementDirection(switchDirection);
+    });
+    DOMQueries.rotateButtons[1].addEventListener("click", () => {
+      const switchDirection =
+        DOMState.getPlayerTwoShipPlacementDirection() === "vertical"
+          ? "horizontal"
+          : "vertical";
+      DOMState.setPlayerTwoShipPlacementDirection(switchDirection);
+    });
+  };
+  return {
+    addShipEventListeners,
+    addFleetSquaresEventListeners,
+    addRotateBtnsEventListeners,
+  };
 };
 
 export { eventHandler };
