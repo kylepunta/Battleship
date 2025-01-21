5a4cbab3c03c97fcc4a9249c32fe54f21a7faeb3
kylepunta <kylepurcell1998@gmail.com>
2025-01-21 13:36:15 +0000

    Created a state module for storing information about the current state of the DOM and the game

---
+++ src/modules/state.js
@@ -0,0 +1,57 @@
+import { Player } from "./player.js";
+import { GameBoard } from "./gameBoard.js";
+import { Ship } from "./ship.js";
+
+const states = (() => {
+  const domState = {
+    draggedPiece: null,
+    droppedPiece: null,
+    rotateMode: "vertical",
+    unitClicked: null,
+  };
+  const gameState = {
+    playerOne: null,
+    playerTwo: null,
+    playerOneGameBoard: null,
+    playerTwoGameBoard: null,
+    playerOneFleetBoard: null,
+    playerOneAttackBoard: null,
+    playerTwoFleetBoard: null,
+    playerTwoAttackBoard: null,
+    currentTurn: "playerOne",
+  };
+  return {
+    domState,
+    gameState,
+    getDraggedPiece: () => domState.draggedPiece,
+    setDraggedPiece: (draggedPiece) => (domState.draggedPiece = draggedPiece),
+    getDroppedPiece: () => domState.droppedPiece,
+    setDroppedPiece: (droppedPiece) => (domState.droppedPiece = droppedPiece),
+    getPlayerOne: () => gameState.playerOne,
+    setPlayerOne: (player) => (gameState.playerOne = player),
+    getPlayerTwo: () => gameState.playerTwo,
+    setPlayerTwo: (player) => (gameState.playerTwo = player),
+    getPlayerOneGameBoard: () => gameState.playerOneGameBoard,
+    setPlayerOneGameBoard: (board) => (gameState.playerOneGameBoard = board),
+    getPlayerTwoGameBoard: () => gameState.playerTwoGameBoard,
+    setPlayerTwoGameBoard: (board) => (gameState.playerTwoGameBoard = board),
+    getPlayerOneFleetBoard: () => gameState.playerOneFleetBoard,
+    setPlayerOneFleetBoard: (board) => (gameState.playerOneFleetBoard = board),
+    getPlayerOneAttackBoard: () => gameState.playerOneAttackBoard,
+    setPlayerOneAttackBoard: (board) =>
+      (gameState.playerOneAttackBoard = board),
+    getPlayerTwoFleetBoard: () => gameState.playerTwoFleetBoard,
+    setPlayerTwoFleetBoard: (board) => (gameState.playerTwoFleetBoard = board),
+    getPlayerTwoAttackBoard: () => gameState.playerTwoAttackBoard,
+    setPlayerTwoAttackBoard: (board) =>
+      (gameState.playerTwoAttackBoard = board),
+    getCurrentTurn: () => gameState.currentTurn,
+    setCurrentTurn: (turn) => (gameState.currentTurn = turn),
+    getRotateMode: () => domState.rotateMode,
+    setRotateMode: (mode) => (domState.rotateMode = mode),
+    getUnitClicked: () => domState.unitClicked,
+    setUnitClicked: (unit) => (domState.unitClicked = unit),
+  };
+})();
+
+export { states };
