4f2c43f581c8597ecbcbad072c10d43e3aae70bc
kylepunta <kylepurcell1998@gmail.com>
2025-01-23 11:33:07 +0000

    Replaced code with event listener for DOMContentLoaded and callback function to add main menu event listeners

--- src/index.js
+++ src/index.js
@@ -6,21 +6,6 @@ import { GameBoard } from "./modules/gameBoard.js";
 import { Ship } from "./modules/ship.js";
 import { states } from "./modules/state.js";
 
-renderDOM.clearPage();
-renderDOM.renderBoards();
-renderDOM.renderBoardMenu();
-renderDOM.renderShipYard();
-addEventListeners();
-
-const playerOne = new Player("Real");
-const playerTwo = new Player("Real");
-states.setPlayerOne(playerOne);
-states.setPlayerTwo(playerTwo);
-states.setPlayerOneGameBoard(playerOne.playerBoard);
-states.setPlayerTwoGameBoard(playerTwo.playerBoard);
-states.setPlayerOneFleetBoard(playerOne.playerBoard.board);
-states.setPlayerOneAttackBoard(playerOne.playerBoard.missedAttacks);
-states.setPlayerTwoFleetBoard(playerTwo.playerBoard.board);
-states.setPlayerTwoAttackBoard(playerTwo.playerBoard.missedAttacks);
-
-console.log(states.getPlayerOneFleetBoard());
+document.addEventListener("DOMContentLoaded", () => {
+  addEventListeners.addMainMenuListeners();
+});
